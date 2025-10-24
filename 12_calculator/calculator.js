785a25072d679cb7fdc966bd66e5e5a80a4abb3c
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-10-24 16:21:17 +0300

    Solve Ex 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, elem) => sum + elem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prod, elem) => prod * elem, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let p = 1;
+
+  for (let i = 1; i <= a; i++)
+    p *= i;
+
+  return p;
 };
 
 // Do not edit below this line
