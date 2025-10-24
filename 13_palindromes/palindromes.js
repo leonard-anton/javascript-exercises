9ccfc2a5af08814bf7aeaeb34e7caeaa37555b26
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-10-24 16:52:32 +0300

    Solve Ex 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    const cleanedString = s.toLowerCase().split('').filter((char) => alphanumerical.includes(char)).join('');
+
+    const reversed = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
