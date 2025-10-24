e10e5f5aae8817c26dd5d8803a2075062e8af73e
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-10-24 17:06:23 +0300

    Solve Ex 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
+    if (n < 0)
+        return "OOPS";
+
+    if (n == 0)
+        return 0;
+
+    if (n < 3)
+        return 1;
+
+    let f1 = 1, f2 = 1, f3, count = 2;
+
+    do {
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+        count++;
+    } while (count < n);
+
+    return f3;
 };
 
 // Do not edit below this line
