2a2263fe2d40418211db127e56d0157cd960e719
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-10-24 17:34:43 +0300

    Solve Ex 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death)
+        death = new Date().getFullYear();
 
+    return death - birth;
+}
+
+const findTheOldest = function (arr) {
+    return arr.reduce((oldest, current) => {
+        oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        if (currentAge > oldestAge) return current;
+        else return oldest;
+    });
 };
 
 // Do not edit below this line
